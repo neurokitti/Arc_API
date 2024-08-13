03d33866ecf26e6b330d810bbea82c9cc3c961e6
Andrew Larson <github@andrew-larson.dev>
2024-08-12 18:24:08 -0700

    Separate example test from the main py file

--- Arc_API.py
+++ Arc_API.py
@@ -196,16 +196,3 @@ class arc_API:
             subprocess.Popen([self.arc_executable])
         else:
             subprocess.Popen(f"osascript -e 'tell application \"{self.arc_executable}\" to activate'", shell=True)
-
-
-
-if __name__ == "__main__":
-    arc_api = arc_API()
-    arc_api.close_arc()
-    arc_api.set_space_theme_color(0,"blendedSingleColor",[(255,0,255,1)],"light")
-    print(arc_api.get_number_of_spaces())
-    arc_api.set_space_name(0, "aaaaaaaaaaaaaaaaaaaaaaaaa")
-    arc_api.set_space_icon(0, '😍')
-    print(arc_api.get_space_name(0))
-    print(arc_api.get_space_theme_type(0))
-    print(arc_api.get_space_theme_data(0))
---
+++ example-test.py
@@ -0,0 +1,12 @@
+import Arc_API
+
+if __name__ == "__main__":
+    arc_api = arc_API()
+    arc_api.close_arc()
+    arc_api.set_space_theme_color(0,"blendedSingleColor",[(255,0,255,1)],"light")
+    print(arc_api.get_number_of_spaces())
+    arc_api.set_space_name(0, "aaaaaaaaaaaaaaaaaaaaaaaaa")
+    arc_api.set_space_icon(0, '😍')
+    print(arc_api.get_space_name(0))
+    print(arc_api.get_space_theme_type(0))
+    print(arc_api.get_space_theme_data(0))
